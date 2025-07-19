2ce3ae5c9209fc2106b7cccca4eac853f82e5439
Muhammad Usman <usman853136@gmail.com>
2025-07-19 20:45:01 +0500

    initial

---
+++ lib/db/schema.ts
@@ -0,0 +1,49 @@
+import { pgTable, text, uuid, integer, boolean, timestamp } from "drizzle-orm/pg-core"
+
+import { relations } from "drizzle-orm"
+
+
+export const files = pgTable("files", {
+    id: uuid("id").defaultRandom().primaryKey(),
+
+    // basic files and folder information
+    name: text("name").notNull(),
+    path: text("path").notNull(),
+    size: integer('size').notNull(),
+    type: text("type").notNull(), // folder
+
+
+
+    // storage information
+
+    fileUrl: text("file_url").notNull(),
+    thumbnailUrl: text("thumbnail_url"),
+
+    // ownership
+
+    userId: text('user_id').notNull(),
+    parentId: text("parent_id"),
+
+    // file/folder flags
+
+    isFolder: boolean("is_folder").default(false).notNull(),
+    isStarred: boolean("is_starred").default(false).notNull(),
+    isTrash: boolean("is_trash").default(false).notNull(),
+
+    // TimeStamps
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+
+})
+
+export const filesRelations = relations(files, ({ one, many }) => (
+    {
+        parent: one(files, {
+            fields: [files.parentId],
+            references: [files.id]
+        }),
+
+        children: many(files)
+
+    }
+))
